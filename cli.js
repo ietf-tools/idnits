e07f7b4f07b0b4e931c8c7725283746508bb68e7
Nicolas Giard <github@ngpixel.com>
2023-03-07 23:44:25 -0500

    feat: basic xml parsing

--- cli.js
+++ cli.js
@@ -2,8 +2,10 @@
 
 import chalk from 'chalk'
 import yargs from 'yargs/yargs'
+import path from 'node:path'
 import { readFile } from 'node:fs/promises'
 import { DateTime } from 'luxon'
+import { checkNits } from './lib/index.mjs'
 
 // Define CLI arguments config
 
@@ -38,8 +40,24 @@ const argv = yargs(process.argv.slice(2))
   .argv
 
 // Get package version
-
 const pkgInfo = JSON.parse(await readFile('./package.json', 'utf8'))
 console.log(chalk.yellowBright.bold(`idnits ${pkgInfo.version}`))
 
-console.info(argv)
+// Read document
+const docPath = path.resolve(process.cwd(), argv.file)
+const docPathObj = path.parse(docPath)
+let docRaw = ''
+try {
+  docRaw = await readFile(docPath, 'utf8')
+} catch (err) {
+  console.error(`Failed to read document: ${err.message}`)
+  process.exit(1)
+}
+
+// Validate document
+try {
+  await checkNits(docRaw, docPathObj.base, {})
+} catch (err) {
+  console.error(`Validation failed: ${err.message}`)
+  process.exit(1)
+}
