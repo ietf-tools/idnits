50df1e2df5d3406f81fa24ea5c3732046ee7e77d
NGPixel <github@ngpixel.com>
2023-03-29 02:52:28 -0400

    feat: xml ipr validations + doc name match validation

---
+++ tests/xml.test.js
@@ -0,0 +1,53 @@
+import { describe, expect, test } from '@jest/globals'
+import { MODES } from '../lib/config/modes.mjs'
+import { toContainError, ValidationComment, ValidationError, ValidationWarning } from '../lib/helpers/error.mjs'
+import { validateIprAttribute } from '../lib/modules/xml.mjs'
+import { baseXMLDoc } from './fixtures/base-doc.mjs'
+import { set } from 'lodash-es'
+
+expect.extend({
+  toContainError
+})
+
+describe('XML document should have a valid ipr attribute', () => {
+  test('valid ipr value', async () => {
+    const doc = { ...baseXMLDoc }
+    set(doc, 'data.rfc.@_ipr', 'trust200902')
+    expect(validateIprAttribute(doc)).toHaveLength(0)
+  })
+  test('missing ipr attribute', async () => {
+    const doc = { ...baseXMLDoc }
+    set(doc, 'data.rfc', {})
+    expect(validateIprAttribute(doc)).toContainError('MISSING_IPR_ATTRIBUTE', ValidationError)
+  })
+  test('invalid ipr attribute', async () => {
+    const doc = { ...baseXMLDoc }
+    // -> Empty value
+    set(doc, 'data.rfc.@_ipr', '')
+    expect(validateIprAttribute(doc)).toContainError('INVALID_IPR_VALUE', ValidationWarning)
+    // -> Invalid value
+    set(doc, 'data.rfc.@_ipr', 'test')
+    expect(validateIprAttribute(doc)).toContainError('INVALID_IPR_VALUE', ValidationWarning)
+  })
+})
+
+describe('XML stream document should only use allowed ipr values', () => {
+  test('valid ipr value', async () => {
+    const doc = { ...baseXMLDoc }
+    set(doc, 'data.rfc.@_ipr', 'trust200902')
+    set(doc, 'data.rfc.@_submissionType', 'IETF')
+    expect(validateIprAttribute(doc)).toHaveLength(0)
+  })
+  test('invalid noModificationTrust200902 ipr attribute', async () => {
+    const doc = { ...baseXMLDoc }
+    set(doc, 'data.rfc.@_ipr', 'noModificationTrust200902')
+    set(doc, 'data.rfc.@_submissionType', 'IETF')
+    expect(validateIprAttribute(doc)).toContainError('FORBIDDEN_IPR_VALUE_FOR_STREAM', ValidationError)
+  })
+  test('invalid noDerivativesTrust200902 ipr attribute', async () => {
+    const doc = { ...baseXMLDoc }
+    set(doc, 'data.rfc.@_ipr', 'noDerivativesTrust200902')
+    set(doc, 'data.rfc.@_submissionType', 'IETF')
+    expect(validateIprAttribute(doc)).toContainError('FORBIDDEN_IPR_VALUE_FOR_STREAM', ValidationError)
+  })
+})
