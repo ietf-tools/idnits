001dda621197938cdb0a4844c8218848e3bd7e33
NGPixel <github@ngpixel.com>
2023-03-29 23:47:18 -0400

    feat: validateIntroductionSection

--- tests/sections.test.js
+++ tests/sections.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, test } from '@jest/globals'
 import { MODES } from '../lib/config/modes.mjs'
 import { toContainError, ValidationComment, ValidationError, ValidationWarning } from '../lib/helpers/error.mjs'
-import { validateAbstractSection } from '../lib/modules/sections.mjs'
+import { validateAbstractSection, validateIntroductionSection } from '../lib/modules/sections.mjs'
 import { baseXMLDoc } from './fixtures/base-doc.mjs'
 import { set } from 'lodash-es'
 
@@ -38,6 +38,37 @@ describe('document should have a valid abstract section', () => {
       // -> Add a valid child but keep the invalid one
       set(doc, 'data.rfc.front.abstract.t', 'test')
       expect(validateAbstractSection(doc)).toContainError('INVALID_ABSTRACT_SECTION_CHILD', ValidationError)
+    })
+  })
+})
+
+describe('document should have a valid introduction section', () => {
+  describe('XML Document Type', () => {
+    test('valid introduction section', async () => {
+      const doc = { ...baseXMLDoc }
+      set(doc, 'data.rfc.middle.section[0].name', 'Introduction')
+      set(doc, 'data.rfc.middle.section[0].t', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+      expect(validateIntroductionSection(doc)).toHaveLength(0)
+    })
+    test('missing introduction section', async () => {
+      const doc = { ...baseXMLDoc }
+      set(doc, 'data.rfc.middle.section', [])
+      expect(validateIntroductionSection(doc)).toContainError('MISSING_INTRODUCTION_SECTION', ValidationError)
+      expect(validateIntroductionSection(doc, { mode: MODES.FORGIVE_CHECKLIST })).toContainError('MISSING_INTRODUCTION_SECTION', ValidationWarning)
+      expect(validateIntroductionSection(doc, { mode: MODES.SUBMISSION })).toHaveLength(0)
+    })
+    test('invalid introduction section', async () => {
+      const doc = { ...baseXMLDoc }
+      set(doc, 'data.rfc.middle.section[0].name', 'Introduction')
+      expect(validateIntroductionSection(doc)).toContainError('INVALID_INTRODUCTION_SECTION', ValidationError)
+    })
+    test('invalid introduction section children', async () => {
+      const doc = { ...baseXMLDoc }
+      // -> Invalid child element
+      set(doc, 'data.rfc.middle.section[0].name', 'Introduction')
+      set(doc, 'data.rfc.middle.section[0].t', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+      set(doc, 'data.rfc.middle.section[0].abc', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+      expect(validateIntroductionSection(doc)).toContainError('INVALID_INTRODUCTION_SECTION_CHILD', ValidationError)
     })
   })
 })
