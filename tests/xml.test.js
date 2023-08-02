0a95a059336a5149117ce512ef72311c5404c5d0
NGPixel <github@ngpixel.com>
2023-08-01 20:40:17 -0400

    feat: validate code blocks

--- tests/xml.test.js
+++ tests/xml.test.js
@@ -1,6 +1,7 @@
 import { describe, expect, test } from '@jest/globals'
+import { MODES } from '../lib/config/modes.mjs'
 import { toContainError, ValidationError, ValidationWarning } from '../lib/helpers/error.mjs'
-import { detectDeprecatedElements, validateIprAttribute } from '../lib/modules/xml.mjs'
+import { detectDeprecatedElements, validateCodeBlocks, validateIprAttribute } from '../lib/modules/xml.mjs'
 import { baseXMLDoc } from './fixtures/base-doc.mjs'
 import { cloneDeep, set } from 'lodash-es'
 
@@ -67,5 +68,27 @@ describe('XML stream document should only use allowed ipr values', () => {
     set(doc, 'data.rfc._attr.ipr', 'noDerivativesTrust200902')
     set(doc, 'data.rfc._attr.submissionType', 'IETF')
     await expect(validateIprAttribute(doc)).resolves.toContainError('FORBIDDEN_IPR_VALUE_FOR_STREAM', ValidationError)
+  })
+})
+
+describe('XML document should not contain <CODE BEGINS> tags', () => {
+  test('valid sourcecode value', async () => {
+    const doc = cloneDeep(baseXMLDoc)
+    set(doc, 'data.rfc.sourcecode', 'test')
+    await expect(validateCodeBlocks(doc)).resolves.toHaveLength(0)
+  })
+  test('sourcecode with a <CODE BEGINS>', async () => {
+    const doc = cloneDeep(baseXMLDoc)
+    set(doc, 'data.rfc.sourcecode', '<CODE BEGINS>test')
+    await expect(validateCodeBlocks(doc)).resolves.toContainError('UNNECESSARY_CODE_BEGINS', ValidationWarning)
+    await expect(validateCodeBlocks(doc, { mode: MODES.FORGIVE_CHECKLIST })).resolves.toContainError('UNNECESSARY_CODE_BEGINS', ValidationWarning)
+    await expect(validateCodeBlocks(doc, { mode: MODES.SUBMISSION })).resolves.toHaveLength(0)
+  })
+  test('<CODE BEGINS> without <sourcecode> tag', async () => {
+    const doc = cloneDeep(baseXMLDoc)
+    set(doc, 'data.rfc.t', '<CODE BEGINS>test')
+    await expect(validateCodeBlocks(doc)).resolves.toContainError('MISSING_SOURCECODE_TAG', ValidationWarning)
+    await expect(validateCodeBlocks(doc, { mode: MODES.FORGIVE_CHECKLIST })).resolves.toContainError('MISSING_SOURCECODE_TAG', ValidationWarning)
+    await expect(validateCodeBlocks(doc, { mode: MODES.SUBMISSION })).resolves.toHaveLength(0)
   })
 })
