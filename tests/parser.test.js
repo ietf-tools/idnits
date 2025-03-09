2a377ddb93bd89143cb0ba42abcae98f11c895a8
Dmutre <104783173+Dmutre@users.noreply.github.com>
2025-03-09 02:06:43 +0100

    feat: version of document is unexpected (already exists, or leaves a gap) (#92)
    * feat/version-of-document-is-unexpected
    
    * feat: added validate txt date
    
    ---------
    
    Co-authored-by: Vladimir <walker7732@gmail.com>
    Co-authored-by: Nicolas Giard <github@ngpixel.com>

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -944,3 +944,25 @@ describe('Parsing IANA considerations section', () => {
     expect(result.data.content.ianaConsiderations).toBe(null)
   })
 })
+
+describe('Parse document slug', () => {
+  test('Parse document slug correctly', async () => {
+    const txt = `
+      ${metaTXTBlock}
+      ${tableOfContentsTXTBlock}
+    `
+
+    const result = await parse(txt, 'txt')
+    expect(result.data.slug).toBe('draft-ietf-idr-rt-derived-community-05')
+  })
+
+  test('Parse document without slug correctly', async () => {
+    const txt = `
+      ${metaTXTBlock.replace('draft-ietf-idr-rt-derived-community-05', '')}
+      ${tableOfContentsTXTBlock}
+    `
+
+    const result = await parse(txt, 'txt')
+    expect(result.data.slug).toBe(null)
+  })
+})
