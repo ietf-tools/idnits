d5d3beaf0cf0fca5596eca4533d20d5cd1b0b0d4
NGPixel <github@ngpixel.com>
2025-08-31 00:39:14 -0400

    refactor(tests): exclude some files from coverage

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
     coverage: {
       enabled: true,
       include: ['lib/**/*.mjs'],
+      exclude: ['lib/index.mjs', 'lib/helpers/*.mjs', 'lib/parsers/xml.mjs', 'lib/remote/*.mjs'],
       provider: 'v8'
     },
     include: ['tests/*.test.js']
